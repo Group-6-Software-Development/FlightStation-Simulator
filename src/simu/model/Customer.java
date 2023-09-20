253a6446d074767ccf85b7d761eb210f3c06a19c
Kim Löfgren <lofgrenkim@outlook.com>
2023-09-20 14:13:38 +0300

    cleaned up Customer from unnecessary booleans.

--- src/simu/model/Customer.java
+++ src/simu/model/Customer.java
@@ -9,7 +9,7 @@ public class Customer {
     private final int id;
     private static int nextAvailableId = 1;
     private static double totalCustomerStayTime = 0;
-    private boolean isPriority, isOnlineCheckedIn, isNormalCheckedIn, isSelfCheckedIn, isBaggageDropped, isSecurityChecked, isLateFromFlight, hasBaggage;
+    private boolean isOnlineCheckedIn, isLateFromFlight, hasBaggage;
 
     private boolean flyOutOfEurope;
 
@@ -17,12 +17,7 @@ public class Customer {
         this.id = nextAvailableId++;
         this.arrivalTime = Clock.getInstance().getClock();
         this.hasBaggage = false; // TODO: needs arrival method to determine if customer has baggage
-        // this.isPriority = false; // TODO: needs method to determine if customer is priority
         this.isOnlineCheckedIn = false; // TODO: needs method to determine if customer is online checked in
-        // this.isSelfCheckedIn = false; // TODO: needs method to determine if customer is self checked in
-        // this.isNormalCheckedIn = false; // TODO: needs servicePoint to flip this to true when customer is checked in
-        // this.isBaggageDropped = false; // TODO: servicePoint needs to flip this to true when customer has dropped baggage
-        // this.isSecurityChecked = false; // TODO: servicePoint needs to flip this to true when customer is security checked
         this.isLateFromFlight = false; // TODO: needs ServicePoint to determine if customer is late from flight
 
         Trace.out(Trace.Level.INFO, "New Customer nbr " + id + " arrived at " + arrivalTime);
@@ -79,13 +74,7 @@ public class Customer {
     }
 
     // Boolean getters and setters
-    public boolean isPriority() {
-        return isPriority;
-    }
 
-    public void setPriority(boolean isPriority) {
-        this.isPriority = isPriority;
-    }
 
     public boolean isOnlineCheckedIn() {
         return isOnlineCheckedIn;
@@ -95,37 +84,6 @@ public class Customer {
         this.isOnlineCheckedIn = isOnlineCheckedIn;
     }
 
-    public boolean isSelfCheckedIn() {
-        return isSelfCheckedIn;
-    }
-
-    public void setSelfCheckedIn(boolean isSelfCheckedIn) {
-        this.isSelfCheckedIn = isSelfCheckedIn;
-    }
-
-    public boolean isNormalCheckedIn() {
-        return isNormalCheckedIn;
-    }
-
-    public void setNormalCheckedIn(boolean normalCheckedIn) {
-        this.isNormalCheckedIn = normalCheckedIn;
-    }
-
-    public boolean isBaggageDropped() {
-        return isBaggageDropped;
-    }
-
-    public void setBaggageDropped(boolean baggageDropped) {
-        isBaggageDropped = baggageDropped;
-    }
-
-    public boolean isSecurityChecked() {
-        return isSecurityChecked;
-    }
-
-    public void setSecurityChecked(boolean securityChecked) {
-        isSecurityChecked = securityChecked;
-    }
 
     public boolean isLateFromFlight() {
         return isLateFromFlight;
@@ -153,12 +111,7 @@ public class Customer {
                 "arrivalTime=" + arrivalTime +
                 ", departureTime=" + departureTime +
                 ", id=" + id +
-                ", isPriority=" + isPriority +
                 ", isOnlineCheckedIn=" + isOnlineCheckedIn +
-                ", isNormalCheckedIn=" + isNormalCheckedIn +
-                ", isSelfCheckedIn=" + isSelfCheckedIn +
-                ", isBaggageDropped=" + isBaggageDropped +
-                ", isSecurityChecked=" + isSecurityChecked +
                 ", isLateFromFlight=" + isLateFromFlight +
                 ", hasBaggage=" + hasBaggage +
                 '}';

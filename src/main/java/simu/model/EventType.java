4e761a08c95bfbb5b90d974b9b9c56d40df7b1c4
Kim Löfgren <lofgrenkim@outlook.com>
2023-10-06 18:48:04 +0300

    EventName return

--- src/main/java/simu/model/EventType.java
+++ src/main/java/simu/model/EventType.java
@@ -3,5 +3,23 @@ package simu.model;
 import simu.framework.IEventType;
 
 public enum EventType implements IEventType{
-    ARR1, CHECKIN, BAGDROP, SECURITYCHECK, RANDOMINSPECTION, PASSPORTCHECK, TICKETINSPECTION
+    ARR1, CHECKIN, BAGDROP, SECURITYCHECK, RANDOMINSPECTION, PASSPORTCHECK, TICKETINSPECTION;
+
+
+    /**
+     * Returns the name of the EventType as a String
+     * @param type EventType which name is wanted as a string
+     * @return String of the EventTypes name
+     */
+    public static String eventName(EventType type) {
+        return switch (type) {
+            case ARR1 -> "ARRIVAL";
+            case CHECKIN -> "CHECK-IN";
+            case BAGDROP -> "BAG-DROP";
+            case SECURITYCHECK -> "SECURITY CHECK";
+            case RANDOMINSPECTION -> "RANDOM INSPECTION";
+            case PASSPORTCHECK -> "PASSPORT CHECK";
+            case TICKETINSPECTION -> "TICKET INSPECTION";
+        };
+    }
 }

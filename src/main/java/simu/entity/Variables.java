6159a2fd688ab432528fae8144e9b1297702dbe2
Kim Löfgren <lofgrenkim@outlook.com>
2023-10-08 17:22:11 +0300

    imports fixed to specific needs of class

--- src/main/java/simu/entity/Variables.java
+++ src/main/java/simu/entity/Variables.java
@@ -1,6 +1,11 @@
 package simu.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "variables")
